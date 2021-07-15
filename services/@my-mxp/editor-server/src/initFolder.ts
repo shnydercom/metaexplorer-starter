d0d501641d8601796e29ca3ddc17854dccda22c8
shnyder <ajs.private@gmail.com>
2021-07-16 01:43:43 +0200

    build fixed

--- services/@my-mxp/editor-server/src/initFolder.ts
+++ services/@my-mxp/editor-server/src/initFolder.ts
@@ -9,8 +9,8 @@ if (!fs.existsSync(path.resolve(devNocodeFolder))) {
 	mkdir(devNocodeFolder);
 	mkdir(devNocodeFolder + "/blocks");
 	mkdir(devNocodeFolder + "/" + DYN_USER_CSS_FOLDER);
-
 	//copy nocode to devNocodeFolder
-	const nocodePath = process.env.NOCODE_BLOCKS_PATH ? process.env.NOCODE_BLOCKS_PATH : '../../nocode/metaexplorer.io/blocks';
+	const nocodePath = process.env.NOCODE_BLOCKS_PATH ? process.env.NOCODE_BLOCKS_PATH : '../../../nocode/@my-mxp/jsonstore/blocks';
 	copyDir(path.resolve(nocodePath.trim()), path.resolve(devNocodeFolder, "blocks"));
+
 }
